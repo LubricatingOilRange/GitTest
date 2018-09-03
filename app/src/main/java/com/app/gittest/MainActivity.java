73f4d318c21eda1fd1a42726444a24e3596f5e0e
Ruibing.han <hanrb26@163.com>
2018-09-03 23:13:35 +0800

    ssss

--- app/src/main/java/com/app/gittest/MainActivity.java
+++ app/src/main/java/com/app/gittest/MainActivity.java
@@ -1,5 +1,8 @@
 package com.app.gittest;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
          CommonDialogFragment.newInstance(0, R.style.anim_dialog, new CommonDialogFragment.OnDialogCallBack() {
              @Override
              public void onConfirm() {
+                 startActivity( getAppDetailSettingIntent());
+                ;
                  Toast.makeText(getApplicationContext(),"确认了",Toast.LENGTH_SHORT).show();
              }
 
@@ -21,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
                  Toast.makeText(getApplicationContext(),"取消了",Toast.LENGTH_SHORT).show();
              }
          }).show(getSupportFragmentManager(),"");
+    }
+    private Intent getAppDetailSettingIntent() {
+        Intent localIntent = new Intent();
+        localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= 9) {
+            localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            localIntent.setData(Uri.fromParts("package", getPackageName(), null));
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            localIntent.setAction(Intent.ACTION_VIEW);
+            localIntent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+            localIntent.putExtra("com.android.settings.ApplicationPkgName", getPackageName());
+        }
+        return localIntent;
     }
 }
